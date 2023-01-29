1ac7fee9fc2655a20d8ed7e922034097997a11be
MichalPokojny <michal.pokojny@hotmail.com>
2023-01-29 22:44:23 +0000

    final deployment

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('test_blog.urls'), name='blog_urls')
+    path('', include('test_blog.urls'), name='blog_urls'),
+    path('accounts/', include('allauth.urls')),
+    
     
 ]
